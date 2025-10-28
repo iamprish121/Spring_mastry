dffcf2ffbcb7fcd2da9b3f162f4eafae599d063f
PRIYANSHU ANAND <122816376+iamprish121@users.noreply.github.com>
2025-10-28 21:48:31 +0530

    add lombok for reduce boilerplate

--- src/main/java/journalApp/Entity/JournalEntry.java
+++ src/main/java/journalApp/Entity/JournalEntry.java
@@ -1,11 +1,13 @@
 package journalApp.Entity;
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
 
+@Data
 @Document(collection = "journal_entries")
 public class JournalEntry{
 
@@ -15,36 +17,36 @@ public class JournalEntry{
     private String content;
     private LocalDate date;
 
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTittle() {
-        return tittle;
-    }
-
-    public void setTittle(String tittle) {
-        this.tittle = tittle;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
+//    public LocalDate getDate() {
+//        return date;
+//    }
+//
+//    public void setDate(LocalDate date) {
+//        this.date = date;
+//    }
+//
+//    public ObjectId getId() {
+//        return id;
+//    }
+//
+//    public void setId(ObjectId id) {
+//        this.id = id;
+//    }
+//
+//    public String getTittle() {
+//        return tittle;
+//    }
+//
+//    public void setTittle(String tittle) {
+//        this.tittle = tittle;
+//    }
+//
+//    public String getContent() {
+//        return content;
+//    }
+//
+//    public void setContent(String content) {
+//        this.content = content;
+//    }
 
 }
