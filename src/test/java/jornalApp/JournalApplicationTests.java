b8e6cc6d730077a9ea756b39dcbf727b4f0136f2
PRIYANSHU ANAND <122816376+iamprish121@users.noreply.github.com>
2025-11-09 14:44:45 +0530

    Properties : command line arguments pass via terminal and edit configuration

---
+++ src/test/java/jornalApp/JournalApplicationTests.java
@@ -0,0 +1,15 @@
+package jornalApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+public class JournalApplicationTests {
+
+    @SpringBootTest
+    class JournalAppApplicationTests {
+
+        @Test
+        void contextLoads() {
+        }
+    }
+}
