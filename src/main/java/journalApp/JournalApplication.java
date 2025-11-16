6138f9df4e9e8f90f3e79d519f63b683d3c18a4a
PRIYANSHU ANAND <122816376+iamprish121@users.noreply.github.com>
2025-11-16 12:57:21 +0530

    Profiles

--- src/main/java/journalApp/JournalApplication.java
+++ src/main/java/journalApp/JournalApplication.java
@@ -3,7 +3,9 @@ package journalApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -14,6 +16,9 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class JournalApplication {
 
     public static void main(String[] args) {
+//      ConfigurableApplicationContext context =  SpringApplication.run(JournalApplication.class, args);
+//      ConfigurableEnvironment environment = context.getEnvironment();
+//      System.out.println("Application Running Environment:"+ environment.getActiveProfiles()[0]);
         SpringApplication.run(JournalApplication.class, args);
     }
 
