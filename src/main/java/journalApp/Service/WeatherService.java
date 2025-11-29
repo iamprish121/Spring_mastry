3c9ee3126f4198158a59161f0de9ad24f13e9a93
PRIYANSHU ANAND <122816376+iamprish121@users.noreply.github.com>
2025-11-29 23:58:23 +0530

    @Value  retrieve values from application.properties

--- src/main/java/journalApp/Service/WeatherService.java
+++ src/main/java/journalApp/Service/WeatherService.java
@@ -2,6 +2,7 @@ package journalApp.Service;
 
 import journalApp.Api.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -9,7 +10,9 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class WeatherService {
-    private static final String api_key = "Enter-Your-key-here";
+
+    @Value("${weather.api.key}")
+    private String api_key;
 
     private static final String API = "https://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
 
