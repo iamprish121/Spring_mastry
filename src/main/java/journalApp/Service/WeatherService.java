9b94926347c792e6cddee02e4ab7c0277c456118
PRIYANSHU ANAND <122816376+iamprish121@users.noreply.github.com>
2025-11-29 19:39:01 +0530

    External Post API

--- src/main/java/journalApp/Service/WeatherService.java
+++ src/main/java/journalApp/Service/WeatherService.java
@@ -1,7 +1,10 @@
 package journalApp.Service;
 
 import journalApp.Api.WeatherResponse;
+import journalApp.Entity.UserEntry;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -9,7 +12,7 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class WeatherService {
-   private static final String api_key = "Enter-Your-key-here";
+    private static final String api_key = "Enter-Your-key-here";
 
     private static final String API = "https://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
 
@@ -23,4 +26,22 @@ public class WeatherService {
         return body;
     }
 
+//    public WeatherResponse  postWeather(String city){
+//        String finalApi = API.replace("CITY",city).replace("API_KEY",api_key);
+//
+//        String requestBody = "{\n" +
+//                "    \"userName\":\"test\",\n" +
+//                "    \"password\":\"test\"\n" +
+//                "}";
+//        UserEntry user = UserEntry.builder().userName("username").password("password").build();
+//        HttpHeaders httpHeaders = new HttpHeaders();
+//        httpHeaders.set("key","value");
+////        HttpEntity<String> httpEntity = new HttpEntity<>(requestBody);
+////        HttpEntity<UserEntry>  httpEntity = new HttpEntity<>(user);
+//        HttpEntity<UserEntry> httpEntity = new HttpEntity<>(user,httpHeaders);
+//
+//        ResponseEntity<WeatherResponse> response = restTemplate.exchange(finalApi, HttpMethod.POST, httpEntity, WeatherResponse.class);
+//        WeatherResponse body = response.getBody();
+//        return body;
+//    }
 }
